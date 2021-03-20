54ab953d70fb34a203e34586f5ffe369becca920
LordOfPolls <ddavidallen13@gmail.com>
2021-03-20 15:32:43 +0000

    init project

---
+++ main.py
@@ -0,0 +1,43 @@
+import logging
+import os
+import typing
+from time import sleep
+
+from source import utilities
+
+log: typing.Union[logging.Logger, None] = None
+
+
+def sanityChecks() -> bool:
+    try:
+        if not os.path.exists("data"):
+            os.makedirs("data")
+    except Exception as e:
+        print(e)
+        return False
+    return True
+
+
+def main():
+    from source import bot
+    log.info("Ready, calling bot.py")
+    bot.run()
+
+
+if __name__ == '__main__':
+    logo = r"""
+__________        .__              .___.__        
+\______   \_____  |  | _____     __| _/|__| ____  
+ |     ___/\__  \ |  | \__  \   / __ | |  |/    \ 
+ |    |     / __ \|  |__/ __ \_/ /_/ | |  |   |  \
+ |____|    (____  /____(____  /\____ | |__|___|  /
+                \/          \/      \/         \/ """
+
+    logo = logo.replace("r", "\033[0m")
+    logo = logo.replace("c", "\033[96m")
+    print(logo)
+    sleep(1)
+    sanityChecks()
+    log = utilities.getLog("Main")
+    log.info("Logging system started")
+    main()
---
+++ source/utilities.py
@@ -0,0 +1,70 @@
+import logging
+import pickle
+
+import colorlog
+from colorlog import ColoredFormatter
+from concurrent.futures.thread import ThreadPoolExecutor
+
+import source.pagination as pagination
+
+paginator = pagination
+
+from discord.ext import commands
+
+thread_pool = ThreadPoolExecutor(max_workers=2)  # a thread pool
+
+discordCharLimit = 2000
+
+logging.SPAM = 9
+logging.addLevelName(logging.SPAM, "SPAM")
+
+
+def spam(self, message, *args, **kws):
+    self._log(logging.SPAM, message, args, **kws)
+
+
+logging.Logger.spam = spam
+
+
+def getLog(filename, level=logging.DEBUG) -> logging:
+    """ Sets up logging, to be imported by other files """
+    streamHandler = colorlog.StreamHandler()
+    streamFormatter = ColoredFormatter(
+        "{asctime} {log_color}|| {levelname:^8} || {name:^11s} || {reset}{message}",
+        datefmt="%H:%M:%S",
+        reset=True,
+        log_colors={
+            'DEBUG': 'cyan',
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'red,bg_yellow',
+            'SPAM': 'purple'
+        },
+        secondary_log_colors={},
+        style='{'
+    )
+
+    streamHandler.setLevel(level)
+    streamHandler.setFormatter(streamFormatter)
+
+    _log = colorlog.getLogger(filename)
+
+    _log.addHandler(streamHandler)
+    _log.setLevel(logging.DEBUG)
+    return _log
+
+
+log = getLog("utils")
+
+
+def getToken():
+    try:
+        file = open("data/token.pkl", "rb")
+        token = pickle.load(file)
+    except:  # if it cant load the token, ask for one, and then pickle it
+        file = open("data/token.pkl", "wb")
+        token = input("Input Discord Token: ").strip()
+        pickle.dump(token, file)
+    file.close()
+    return token

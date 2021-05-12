b2229c05ec03af9aa3c011b272242b9f787cf9f9
LordOfPolls <ddavidallen13@gmail.com>
2021-05-12 12:14:50 +0100

    jank version of sync_all_commands to only update on guild

--- source/cogs/config.py
+++ source/cogs/config.py
@@ -5,15 +5,15 @@ import typing
 
 import discord
 from discord.ext import commands
-from discord_slash import cog_ext, SlashContext
+from discord_slash import cog_ext, SlashContext, model
 from discord_slash.utils import manage_commands
 
 from source import utilities, dataclass, shared
 
-log: logging.Logger = utilities.getLog("Cog::LinkDetect")
+log: logging.Logger = utilities.getLog("Cog::Config")
 
 
-class LinkDetection(commands.Cog):
+class Config(commands.Cog):
     """Configuration commands"""
 
     def __init__(self, bot):
@@ -118,7 +118,8 @@ class LinkDetection(commands.Cog):
 
             guild_data.moderation_roles.append(role.id)
             await self.bot.redis.set(guild_data.key, guild_data.to_json())
-            await self.bot.add_permissions_to_commands(True)
+            await self.bot.add_permissions_to_commands()
+            await self.sync_all_commands(ctx.guild_id)
 
             await ctx.send(
                 f"Commands are now enabled for {role.mention}\n**Note:**It may take a few minutes for discord to show this change",
@@ -146,7 +147,8 @@ class LinkDetection(commands.Cog):
                 )
             guild_data.moderation_roles.remove(role.id)
             await self.bot.redis.set(guild_data.key, guild_data.to_json())
-            await self.bot.add_permissions_to_commands(True)
+            await self.bot.add_permissions_to_commands()
+            await self.sync_all_commands(ctx.guild_id)
 
             await ctx.send(
                 f"Commands are now disabled for {role.mention}\n**Note:**It may take a few minutes for discord to show this change",
@@ -164,15 +166,76 @@ class LinkDetection(commands.Cog):
             log.error("".join(traceback.format_exception(type(error), error, error.__traceback__)))
             await ctx.send("An error occurred trying to run this command, it has been logged. Please try again later")
 
+    async def sync_all_commands(self, guild_id: int):
+        """
+        gutted version of slash.sync_all_commands to only modify one guild for permissions
+
+        not efficient, but it works, and i didnt want to re-engineer it completely,
+        seeing as itll likely be added to the lib eventually
+        """
+        permissions_map = {}
+        cmds = await self.bot.slash.to_dict()
+        cmds_formatted = {None: cmds["global"]}
+        for guild in cmds["guild"]:
+            cmds_formatted[guild] = cmds["guild"][guild]
+
+        for scope in cmds_formatted:
+            permissions = {}
+            new_cmds = cmds_formatted[scope]
+            existing_cmds = await self.bot.slash.req.get_all_commands(guild_id=scope)
+            for command in new_cmds:
+                cmd_name = command["name"]
+                permissions[cmd_name] = command.pop("permissions")
+
+            id_name_map = {}
+            for cmd in existing_cmds:
+                id_name_map[cmd["name"]] = cmd["id"]
+
+            for cmd_name in permissions:
+                cmd_permissions = permissions[cmd_name]
+                cmd_id = id_name_map[cmd_name]
+                for applicable_guild in cmd_permissions:
+                    if applicable_guild not in permissions_map:
+                        permissions_map[applicable_guild] = []
+                    permission = {
+                        "id": cmd_id,
+                        "guild_id": applicable_guild,
+                        "permissions": cmd_permissions[applicable_guild],
+                    }
+                    permissions_map[applicable_guild].append(permission)
+
+        existing_perms = await self.bot.slash.req.get_all_guild_commands_permissions(guild_id)
+        new_perms = permissions_map[guild_id]
+        changed = False
+        if len(existing_perms) != len(new_perms):
+            changed = True
+        else:
+            existing_perms_model = {}
+            for existing_perm in existing_perms:
+                existing_perms_model[existing_perm["id"]] = model.GuildPermissionsData(**existing_perm)
+            for new_perm in new_perms:
+                if new_perm["id"] not in existing_perms_model:
+                    changed = True
+                    break
+                if existing_perms_model[new_perm["id"]] != model.GuildPermissionsData(**new_perm):
+                    changed = True
+                    break
+
+        if changed:
+            log.debug(f"Detected permissions changes on {guild_id}, updating them")
+            await self.bot.slash.req.update_guild_commands_permissions(guild_id, new_perms)
+        else:
+            log.debug(f"Detected no permissions changes on {guild_id}, skipping")
+
 
 def setup(bot):
     """Called when this cog is mounted"""
-    bot.add_cog(LinkDetection(bot))
-    log.info("LinkDetection mounted")
+    bot.add_cog(Config(bot))
+    log.info("Config mounted")
 
 
 def teardown(bot):
     """Called when this cog is unmounted"""
-    log.warning("LinkDetection un-mounted")
+    log.warning("Config un-mounted")
     for handler in log.handlers[:]:
         log.removeHandler(handler)

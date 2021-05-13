baca58845a5f63bd97ebae2d4e07577a338c99b0
LordOfPolls <ddavidallen13@gmail.com>
2021-05-13 08:25:56 +0100

    squish permissions into one cmd

--- source/cogs/listPermissions.py
+++ source/cogs/listPermissions.py
@@ -22,70 +22,76 @@ class ListPermissions(commands.Cog):
 
         self.paginator = pagination.LinePaginator(prefix="", suffix="")
 
-    @cog_ext.cog_subcommand(**jsonManager.getDecorator("guild.role.permissions"))
-    async def role_perms_guild(self, ctx: SlashContext, role: discord.Role):
-        data = []
-        for perm, value in role.permissions:
-            data.append(f"{'✅' if value else '❌'} {perm.replace('_', ' ').title()}")
-        await self.paginator.paginate(
-            data,
-            ctx,
-            embed=discord.Embed(title=f"Guild Permissions For {role.name}", colour=discord.Colour.blurple()),
-            max_lines=10,
-        )
-
-    @cog_ext.cog_subcommand(**jsonManager.getDecorator("channel.role.permissions"))
-    async def role_perms_channel(
-        self,
-        ctx: SlashContext,
-        role: discord.Role,
-        channel: typing.Union[discord.TextChannel, discord.VoiceChannel, discord.CategoryChannel] = None,
-    ):
-        if not channel:
-            channel = ctx.channel
-        data = []
-
-        for perm, value in channel.overwrites_for(role):
-            data.append(f"{'✅' if value else '❌'} {perm.replace('_', ' ').title()}")
-        await self.paginator.paginate(
-            data,
-            ctx,
-            embed=discord.Embed(
-                title=f"Permissions For {role.name} in {channel.name}", colour=discord.Colour.blurple()
+    @cog_ext.cog_subcommand(
+        base="permissions",
+        name="get",
+        description="Get the permissions of a role or user",
+        base_default_permission=True,
+        options=[
+            manage_commands.create_option(
+                name="scope",
+                description="Do you want guild perms, or channel overrides",
+                required=True,
+                option_type=4,
+                choices=[
+                    manage_commands.create_choice(value=1, name="guild"),
+                    manage_commands.create_choice(value=2, name="channel"),
+                ],
             ),
-            max_lines=10,
-        )
+            manage_commands.create_option(
+                name="target", description="The user or role you want to get perms for", required=True, option_type=9
+            ),
+        ],
+    )
+    async def get_permissions(self, ctx: SlashContext, **kwargs):
+        mention = int(kwargs.get("target"))
 
-    @cog_ext.cog_subcommand(**jsonManager.getDecorator("guild.user.permissions"))
-    async def user_perms_guild(self, ctx: SlashContext, user: discord.Member):
+        if user := ctx.guild.get_member(mention):
+            if kwargs.get("scope") == 1:
+                return await self.perms_guild(ctx, user)
+            return await self.perms_channel(ctx, user)
+
+        elif role := ctx.guild.get_role(mention):
+            if kwargs.get("scope") == 1:
+                return await self.perms_guild(ctx, role)
+            return await self.perms_channel(ctx, role)
+
+        else:
+            await ctx.send("Sorry, i couldn't find that user or role")
+
+    async def perms_guild(self, ctx: SlashContext, mention: typing.Union[discord.Role, discord.Member]):
         data = []
-        for perm, value in user.guild_permissions:
+
+        if isinstance(mention, discord.Member):
+            permissions = mention.guild_permissions
+        else:
+            permissions = mention.permissions
+
+        for perm, value in permissions:
             data.append(f"{'✅' if value else '❌'} {perm.replace('_', ' ').title()}")
         await self.paginator.paginate(
             data,
             ctx,
-            embed=discord.Embed(title=f"Guild Permissions For {user.name}", colour=discord.Colour.blurple()),
+            embed=discord.Embed(title=f"Guild Permissions For {mention.name}", colour=discord.Colour.blurple()),
             max_lines=10,
         )
 
-    @cog_ext.cog_subcommand(**jsonManager.getDecorator("channel.user.permissions"))
-    async def user_perms_channel(
-        self,
-        ctx: SlashContext,
-        user: discord.Member,
-        channel: typing.Union[discord.TextChannel, discord.VoiceChannel, discord.CategoryChannel] = None,
-    ):
-        if not channel:
-            channel = ctx.channel
+    async def perms_channel(self, ctx: SlashContext, mention: typing.Union[discord.Role, discord.Member]):
+        channel = ctx.channel
         data = []
 
-        for perm, value in channel.permissions_for(user):
+        if isinstance(mention, discord.Member):
+            permissions = channel.permissions_for(mention)
+        else:
+            permissions = channel.overwrites_for(mention)
+
+        for perm, value in permissions:
             data.append(f"{'✅' if value else '❌'} {perm.replace('_', ' ').title()}")
         await self.paginator.paginate(
             data,
             ctx,
             embed=discord.Embed(
-                title=f"Permissions For {user.name} in {channel.name}", colour=discord.Colour.blurple()
+                title=f"Permissions For {mention.name} in {channel.name}", colour=discord.Colour.blurple()
             ),
             max_lines=10,
         )
